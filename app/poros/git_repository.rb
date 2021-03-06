17bdad39a5b46bad30c694847bddff0b4a18c34c
Gvieve <gnuebel84@yahoo.com>
2021-03-06 16:58:12 -0700

    Add nager service and poros for public holidays

--- app/poros/git_repository.rb
+++ app/poros/git_repository.rb
@@ -5,29 +5,31 @@ class GitRepository
 
   def initialize
     @github_service = GithubService.new
-    @repo_name ||= repo_name
-    @repo_usernames ||= repo_usernames
-    @repo_commits ||= repo_commits
-    @repo_pull_requests ||= repo_pull_requests
+    @repo_name ||= name
+    @repo_usernames ||= usernames
+    @repo_commits ||= commits
+    @repo_pull_requests ||= pull_requests
   end
 
-  def repo_name
+  private
+
+  def name
     @github_service.get_data("")[:name]
   end
 
-  def repo_usernames
+  def usernames
     @github_service.usernames.map do |key, value|
       key[:login]
     end.uniq[0..2]
   end
 
-  def repo_commits
+  def commits
      @repo_usernames.each_with_object({}) do |username, commits|
       commits[username] = @github_service.commits(username).count
     end
   end
 
-  def repo_pull_requests
+  def pull_requests
      @github_service.pull_requests.count
   end
 end
