b64d953b9b20da4164fe537ef3d635ebb662c372
Gvieve <gnuebel84@yahoo.com>
2021-03-06 11:31:33 -0700

    Create bulk_discounts migration, model, spec, and relationships

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe InvoiceItem, type: :model do
     it {should belong_to :item}
     it {should belong_to :invoice}
     it {should have_many(:transactions).through(:invoice)}
+    it {should have_many(:bulk_discounts).through(:merchant)}
     it {should have_one(:merchant).through(:item)}
     it {should have_one(:customer).through(:invoice)}
   end
