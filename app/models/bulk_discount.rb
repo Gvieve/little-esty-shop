1ead313daec65f86d57f0c92ae5756636513425b
Gvieve <gnuebel84@yahoo.com>
2021-03-06 17:50:21 -0700

    Add tests and validations to bulk_discounts model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,4 +2,8 @@ class BulkDiscount < ApplicationRecord
   belongs_to :merchant
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
+
+  validates_presence_of :name
+  validates_numericality_of :item_threshold
+  validates :percent_discount, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100}
 end
