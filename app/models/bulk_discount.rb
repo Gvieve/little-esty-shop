7f899a6647f605881d999ce05e9efd36bafe93f4
Gvieve <gnuebel84@yahoo.com>
2021-03-06 18:17:12 -0700

    Add tests and validations to bulk_discounts model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,7 +3,7 @@ class BulkDiscount < ApplicationRecord
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
 
-  validates_presence_of :name
+  validates_presence_of :name, :item_threshold, :percent_discount
   validates_numericality_of :item_threshold
   validates :percent_discount,
     numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100}
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe BulkDiscount, type: :model do
 
   describe 'validations' do
     it {should validate_presence_of :name}
+    it {should validate_presence_of :item_threshold}
+    it {should validate_presence_of :percent_discount}
     it {should validate_numericality_of :item_threshold}
     it {should validate_numericality_of(:percent_discount).is_greater_than_or_equal_to(1)}
     it {should validate_numericality_of(:percent_discount).is_less_than_or_equal_to(100)}
